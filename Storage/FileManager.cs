75b989b99e8c9fe6b32c5c8afb7f8f0755b1e32a
anhnbt-it <anhnbt.it@gmail.com>
2021-01-19 15:38:16 +0700

    Fixed return products null

--- Storage/FileManager.cs
+++ Storage/FileManager.cs
@@ -9,14 +9,16 @@ namespace crud_web_application.Storage
         string pathToFile = @"/home/anhnbt/Documents/dotnet/crud-web-application/wwwroot/products.txt";
 
         public List<Product> ReadFromFile() {
-            List<Product> products = new List<Product>();
+            List<Product> products = null;
             try
             {
                 // Open the text file using a stream reader.
                 using (StreamReader sr = new StreamReader(pathToFile)) {
                     string line;
+                    products = new List<Product>();
                     while ((line = sr.ReadLine()) != null) {
                         string[] prod = line.Split(',');
+                        
                         Product product = new Product(Convert.ToInt32(prod[0]), prod[1], Convert.ToDouble(prod[2]));
                         products.Add(product);
                     }
